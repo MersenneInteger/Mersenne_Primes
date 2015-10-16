cf5f8b1beb002721a971aa8b5a121d7d83f24b3c
nero <aUtomata2.71828@openmailbox.org>
2015-10-16 14:33:53 -0500

    init first

---
+++ prime.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int power(int base, int exponent);
+
+void main(){
+
+  int i,j,k;
+ 
+  for(j = 2; j < 100; j++){	
+       	int i = 2;
+       	for(; i <= j-1; i++){
+	       	if(j % i == 0){
+	       		break;
+	       	}
+       	}
+       	if((i == j && i != 2)){
+	   for(k = 2; k < 10; k++){
+	    if(power(2, k)-1 == j)
+	      printf("%d ", j);
+	     }
+       }
+   }
+}
+int power(int base, int exponent){
+
+  int ans = 1;
+  while(exponent-- > 0)
+    ans = ans * base;
+
+  return ans;
+}
+
